04385589d40de44cc1d73a27aac408ce428a27eb
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-11-15 14:50:07 +1100

    add conditional for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,17 @@ const mysql = require("mysql");
 // To hide password
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 
 // Make connection
 connection.connect(function (err) {
